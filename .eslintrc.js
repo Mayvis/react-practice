deee2c0f8cb620f539d710a6e6c6b33f973cbae9
= <=>
2019-05-01 18:12:40 +0800

    git init and using state and prop practice

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": ["error", 2],
+        "semi": [2, "always"],
+        "object-curly-spacing": ["error", "always", { "objectsInObjects": false }],
+        "react/no-unescaped-entities": 0,
+        "react/prop-types": 0,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    }
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state can only be accessed in class-based component
+  state = {
+    Persons: [
+      { name: 'LiangYu', age: 20 },
+      { name: "Cari", age: 21 },
+      { name: "Yu", age: 22 }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button>Switch Name</button>
+        <Person name={this.state.Persons[0].name} age={this.state.Persons[0].age}/>
+        <Person name={this.state.Persons[1].name} age={this.state.Persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age}/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React app'))
+  }
 }
 
 export default App;
