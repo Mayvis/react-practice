0d06ebe9b87619c56afbb7840f0706eba8b84d9f
= <=>
2019-05-01 19:28:32 +0800

    add method to handle onclick and add eslint config allows console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": ["error", 2],
-        "semi": [2, "always"],
-        "object-curly-spacing": ["error", "always", { "objectsInObjects": false }],
-        "react/no-unescaped-entities": 0,
-        "react/prop-types": 0,
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
-    }
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": ["error", 2],
+    "semi": [2, "always"],
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "object-curly-spacing": ["error", "always", { "objectsInObjects": false }],
+    "react/no-unescaped-entities": 0,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
 };
--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import './App.css';
 class App extends Component {
   // state can only be accessed in class-based component
   state = {
-    Persons: [
+    persons: [
       { name: 'LiangYu', age: 20 },
       { name: "Cari", age: 21 },
       { name: "Yu", age: 22 }
     ]
+  };
+
+  // method to handle onClick event
+  switchNameHandler = () => {
+    console.log('switchName');
+
+    this.setState({
+      persons: [
+        { name: 'Liang', age: 20 },
+        { name: "Cari", age: 21 },
+        { name: "Yu", age: 22 }
+      ]
+    });
   };
 
   render() {
@@ -17,10 +30,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
-        <Person name={this.state.Persons[0].name} age={this.state.Persons[0].age}/>
-        <Person name={this.state.Persons[1].name} age={this.state.Persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m a React app'))
