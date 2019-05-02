6bdc3a7ca19e45b92eab467a2387b3746903b332
= <=>
2019-05-02 17:12:55 +0800

    Using radium to using media query

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
 const Person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
+
   return(
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" value={props.name} onChange={props.changed}/>
@@ -11,4 +18,4 @@ const Person = (props) => {
   );
 };
 
-export default Person;
+export default Radium(Person);
