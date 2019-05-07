ce20e95580e2873b1576bc33657da11a0dd896de
= <=>
2019-05-07 23:36:05 +0800

    React Fragment
    Fragment is as same as Aux compoent

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import btnClasses from './Person.module.css';
+import React, { Component, Fragment } from 'react';
+// import Aux from '../../../hoc/Auxiliary';
+// import btnClasses from './Person.module.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
 
-    return(
-      <div className={btnClasses.Person}>
+    return (
+      <Fragment>
         <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input
           type="text"
           value={this.props.name}
           onChange={this.props.changed}/>
-      </div>
+      </Fragment>
     );
   }
 }
